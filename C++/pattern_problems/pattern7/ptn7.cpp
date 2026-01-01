2ebefc869a3a59b270528050cbdc18ccbaf8c415
yhsunny176 <yeasinulhaq763@gmail.com>
2026-01-01 22:48:24 +0600

    Pattern 7 solution

---
+++ C++/pattern_problems/pattern7/ptn7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printline(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " ";
+    }
+
+    for (int j = 0; j < 2 * i + 1; j++)
+    {
+      cout << "*";
+    }
+    for (int j = 0; j < n - i + 1; j++)
+    {
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  int t;
+  cin >> t;
+  printline(t);
+}
