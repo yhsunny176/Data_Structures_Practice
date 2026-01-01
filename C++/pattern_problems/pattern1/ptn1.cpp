c06ba65a282c63903b5b37fda69d5832ed0fa0cd
yhsunny176 <yeasinulhaq763@gmail.com>
2026-01-01 20:30:06 +0600

    Pattern Problem 1 (Updated Solution)

--- C++/pattern_problems/pattern1/ptn1.cpp
+++ C++/pattern_problems/pattern1/ptn1.cpp
@@ -3,11 +3,17 @@
 using namespace std;
 
 void printline(int n) {
-
+  for (int i = 1; i<=n; i++){
+    for (int j =1; j<=n; j++){
+      cout << "*";
+    }
+    cout << endl;
+  }
 }
 
 int main()
 {
   int t;
   cin >> t;
+  printline(t);
 }
