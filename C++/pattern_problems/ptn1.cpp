01df0df41354a9bc0b9d8c1aa6837fdb8c8b06b3
yhsunny176 <yeasinulhaq763@gmail.com>
2026-01-01 19:11:44 +0600

    DSA-Pattern-Problems

---
+++ C++/pattern_problems/ptn1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+int main()
+{
+  int i, j;
+  for (i = 1; i <= 5; i++)
+  {
+    for (j = 1; j <= 5; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
