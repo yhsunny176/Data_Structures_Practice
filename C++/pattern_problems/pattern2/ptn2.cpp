6c17a36f33a618a3b0528cd23a3838888a4832be
yhsunny176 <yeasinulhaq763@gmail.com>
2026-01-01 21:01:58 +0600

    Pattern 2 Solution

---
+++ C++/pattern_problems/pattern2/ptn2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printline(int n) {
+  for (int i = 1; i<=n; i++){
+    for (int j =1; j<=i; j++){
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  int t;
+  cin >> t;
+  printline(t);
+}
