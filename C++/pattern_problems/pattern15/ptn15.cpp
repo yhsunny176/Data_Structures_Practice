b63c54cba06d5aa54674fba6d8e7e574d62da2d4
yhsunny176 <yeasinulhaq763@gmail.com>
2026-01-02 20:46:28 +0600

    Pattern 15 Solution

---
+++ C++/pattern_problems/pattern15/ptn15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printline(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    char ch = 'A' + i;
+    for (int j = 0; j <= i; j++)
+    {
+      cout << ch;
+    }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  int t;
+  cin >> t;
+  printline(t);
+}
